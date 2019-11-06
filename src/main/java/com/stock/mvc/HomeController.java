7980445814fd77d5bd29434fa460ba81b436d8a1
Patrick GIRAUD <patrickgiraud@hotmail.com>
2019-11-06 19:47:29 +0100

    Add an import

--- src/main/java/com/stock/mvc/HomeController.java
+++ src/main/java/com/stock/mvc/HomeController.java
@@ -9,7 +9,6 @@ import java.util.Locale;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
